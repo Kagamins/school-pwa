4dfed6055a35a9eb2e9fefec182217c75c2ee7c9
Kagamins <nsoulper@gmail.com>
2022-05-29 10:31:43 +0300

    origin

---
+++ next.config.js
@@ -0,0 +1,11 @@
+const withPWA = require("next-pwa");
+/** @type {import('next').NextConfig} */
+
+
+module.exports = withPWA({
+  pwa: {
+    dest: "public",
+    register: true,
+    skipWaiting: true,
+  },
+});
